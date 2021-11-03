21204afc86270bdd9af4ee724cbdec393a548199
Kyle Lierer <36929178+Kyle-L@users.noreply.github.com>
2021-11-03 09:57:22 -0400

    Added a individual message check.

--- classifier/__main__.py
+++ classifier/__main__.py
@@ -14,30 +14,55 @@ class console_colors:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(help='commands', dest='command')
 
-    # Classify
-    parser.add_argument('TrainingSetPath',
+    # Compare
+    convert_parser = subparsers.add_parser("compare", description='Takes a test data in and determine how accurate the classifier is.')
+    convert_parser.add_argument('TrainingSetPath',
                         metavar='training-set-path',
                         type=str,
                         help='The path to the training data set.')
-    parser.add_argument('TestingSetPath',
+    convert_parser.add_argument('TestingSetPath',
                         metavar='testing-set-path',
                         type=str,
                         help='The path to the test data set.')
 
+    # Check
+    convert_parser = subparsers.add_parser("check", description='Takes a single message and determines the probability of it being spam.')
+    convert_parser.add_argument('TrainingSetPath',
+                        metavar='training-set-path',
+                        type=str,
+                        help='The path to the training data set.')
+    convert_parser.add_argument('Message',
+                        metavar='message',
+                        type=str,
+                        help='An individual message')
+
 
     args = parser.parse_args()
 
-    training_path = args.TrainingSetPath
-    test_path = args.TestingSetPath
-    
-    try: 
-        print(console_colors.HEADER + f'Training classifier with training set: {training_path}' + console_colors.ENDC)
-        classifier = Classifier(training_path)
-        print(console_colors.HEADER + f'Classify with test set: {test_path}' + console_colors.ENDC)
-        accuracy = classifier.classify_test_set(test_path)
-        print(console_colors.OKGREEN + f'Spam message where identified with the following accuracy: {accuracy}' + console_colors.ENDC)
-    except:
-        print(console_colors.FAIL + f'Something went wrong training the classifier!' + console_colors.ENDC)
-    
+    if args.command == 'compare':
+        training_path = args.TrainingSetPath
+        test_path = args.TestingSetPath
+        
+        try: 
+            print(console_colors.HEADER + f'Training classifier with training set: {training_path}' + console_colors.ENDC)
+            classifier = Classifier(training_path)
+            print(console_colors.HEADER + f'Classify with test set: {test_path}' + console_colors.ENDC)
+            accuracy = classifier.classify_test_set(test_path)
+            print(console_colors.OKGREEN + f'Spam message where identified with the following accuracy: {accuracy}' + console_colors.ENDC)
+        except:
+            print(console_colors.FAIL + f'Something went wrong training the classifier!' + console_colors.ENDC)
 
+    if args.command == 'check':
+        training_path = args.TrainingSetPath
+        message = args.Message
+        
+        try: 
+            print(console_colors.HEADER + f'Training classifier with training set: {training_path}' + console_colors.ENDC)
+            classifier = Classifier(training_path)
+            print(console_colors.HEADER + f'Classify the message: {message}' + console_colors.ENDC)
+            accuracy = classifier.classify(message)
+            print(console_colors.OKGREEN + f'The probability of the message is (>0.5 is spam): {accuracy}' + console_colors.ENDC)
+        except:
+            print(console_colors.FAIL + f'Something went wrong training the classifier!' + console_colors.ENDC)
